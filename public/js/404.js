6acab3684f5fbc869a6cf575d832fb7b7097047a
t01090929 <t01090929@gmail.com>
2016-06-22 08:55:14 +0800

    add sounds

--- public/js/404.js
+++ public/js/404.js
@@ -1,31 +1,37 @@
 
 var main = function () {
     var initial = $("#plug1").position().left;
+    var activated = false;
+    var zap = new Audio("./audio/electricShock.mp3");
     $("#plug1").draggable({
         axis: "x"
     });
     $("#plug1").draggable({
         drag: function () {
-            if ($("#plug1").position().left - initial > $("body").width() * 0.281)
+            if (!activated && $("#plug1").position().left - initial > $("body").width() * 0.281)
             {
-                $("body").animate({ 'background-color': '#FFA500' }, 'slow');
-                $("#main").fadeOut(500, function () {
-                        $("#main").attr("src", "./image/change.png");
-                    $("#main").fadeIn(500);
-                })
+                activated = true;
+                $("#zap").show();
+                zap.play();
+                setTimeout(plugIn, 500);
+                setTimeout(function () {
+                    window.location = './404.html';
+                }, 2000);
                 return false;
-            }  
+            }
+            else if(activated){
+                return false;
+            }
         }
     });
-
-
-
-
-
-    //debug¥Î
-    $("#main").click(function() {
-        console.log(document.getElementById("#main").style.content);
-    });
+    function plugIn() {
+        $("#zap").hide();
+        $("body").animate({ 'background-color': '#FFA500' }, 'slow');
+        $("#main").fadeOut(500, function () {
+            $("#main").attr("src", "./image/change.png");
+            $("#main").fadeIn(500);
+        })
+    }
 };
 
 $(document).ready(main);
