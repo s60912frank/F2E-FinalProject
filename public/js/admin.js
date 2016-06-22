f8b17fdd49289f6e43871455db61baef23eda776
s60912frank <s60912frank@gmail.com>
2016-06-22 10:20:14 +0800

    admin頁完成

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,19 +1,49 @@
 $(document).ready(function(){
   $('.deleteTopic').click(function(){
-    $.post('/deleteTopic', { topic: $(this).siblings('.topicTitle').text() }, function(){
-      location.reload();
+    var jthis = $(this);
+    $.post('/deleteTopic', { topic: jthis.parent().siblings('.topicTitle').text() }, function(){
+      alert("成功刪除主題");
+      jthis.parent().parent().remove();
     });
   });
 
-  $('.deleteComment').click(function(){
+  $(document).on('click', '.deleteComment', function(){
+    var jthis = $(this);
     var data = {
-      topic: $(this).parent().parent().siblings('.topicTitle').text(),
-      uid: $(this).siblings('.commentById').text(),
-      comment: $(this).siblings('.commentContent').text()
+      topic: jthis.parent().parent().siblings('h2').text(),
+      uid: jthis.siblings('.uid').text(),
+      comment: jthis.siblings('.commentText').text()
     };
-    //console.log(data);
     $.post('/deleteComment', data, function(){
-      location.reload();
+      alert("成功刪除留言");
+      jthis.parent().remove();
     });
+  });
+
+  $('.expand').click(function(){
+    var jthis = $(this);
+    if(jthis.text() == '展開'){
+      $.get('/getComments?title=' + jthis.parent().siblings('h2').text(), function(data){
+        if(data.length > 0){
+          data.forEach(function(comment){
+            var com = $('<div class="comment">');
+            com.append($('<p class="commentText">').text(comment.comment));
+            com.append($('<button class="deleteComment">').text("刪除留言"));
+            com.append($('<p class="uid" style="display:none;">').text(comment.uid));
+          });
+        }
+        else{
+          var com =  $('<div>').text("沒有留言");
+          com.prependTo(jthis.parent().siblings('.comments'));
+        }
+        jthis.text('收起');
+      });
+    }
+    else{
+      jthis.parent().siblings('.comments').children().each(function(){
+        $(this).remove();
+      });
+      jthis.text('展開');
+    }
   });
 });
